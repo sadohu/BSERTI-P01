efca5f923c96836d2ad3e94c06c0e703c50d748a
sadohu <sadohu26@yahoo.com>
2023-12-18 13:29:06 -0500

    [Add] Upload imagen option and manifest permissions

--- app/src/main/java/com/example/session_01/presentation/activity/DetalleAprobacionDocumentoActivity.kt
+++ app/src/main/java/com/example/session_01/presentation/activity/DetalleAprobacionDocumentoActivity.kt
@@ -1,12 +1,21 @@
 package com.example.session_01.presentation.activity
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.session_01.databinding.ActivityDetalleAprobacionDocumentoBinding
 import com.example.session_01.domain.entity.AprobacionDocumento
 
 class DetalleAprobacionDocumentoActivity : AppCompatActivity() {
     private lateinit var binding : ActivityDetalleAprobacionDocumentoBinding
+    private val PICK_IMAGE_REQUEST = 1
+    private val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetalleAprobacionDocumentoBinding.inflate(layoutInflater)
@@ -24,7 +33,7 @@ class DetalleAprobacionDocumentoActivity : AppCompatActivity() {
 
         // Mostrar los datos en los componentes
         binding.tvNumero.text = aprobacionDocumento.NumeroDocumento
-        binding.btnNumero.text = aprobacionDocumento.NumeroDocumento
+//        binding.btnNumero.text = aprobacionDocumento.NumeroDocumento
         binding.tvTipoDocumento.text = aprobacionDocumento.TipoDocumento
         binding.tvAbrevacion.text = aprobacionDocumento.Abreviacion
         binding.tvEstadoItem.text = aprobacionDocumento.EstadoItem
@@ -33,5 +42,27 @@ class DetalleAprobacionDocumentoActivity : AppCompatActivity() {
         binding.tvFechaRegistro.text = aprobacionDocumento.FechaRegistro
         binding.tvHoraRegistro.text = aprobacionDocumento.HoraRegistro
         binding.tvObservacion.text = aprobacionDocumento.Observacion
+
+        initEvents()
+
+    }
+
+    private fun initEvents(){
+        binding.btnImagen.setOnClickListener {
+            // Antes de abrir la galería, verifica si tienes el permiso
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+                // Si no tienes el permiso, solicítalo al usuario
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+                )
+            } else {
+                // Si ya tienes el permiso, abre la galería
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent, PICK_IMAGE_REQUEST)
+            }
+        }
     }
 }
