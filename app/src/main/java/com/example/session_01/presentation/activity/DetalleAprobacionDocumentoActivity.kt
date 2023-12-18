6e1b34a45cc19d7fcac3330ed25862c42cc16393
sadohu <sadohu26@yahoo.com>
2023-12-18 13:36:23 -0500

    [Add] Capture and convert image to Base64

--- app/src/main/java/com/example/session_01/presentation/activity/DetalleAprobacionDocumentoActivity.kt
+++ app/src/main/java/com/example/session_01/presentation/activity/DetalleAprobacionDocumentoActivity.kt
@@ -1,18 +1,23 @@
 package com.example.session_01.presentation.activity
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Base64
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.session_01.databinding.ActivityDetalleAprobacionDocumentoBinding
 import com.example.session_01.domain.entity.AprobacionDocumento
+import java.io.ByteArrayOutputStream
 
 class DetalleAprobacionDocumentoActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityDetalleAprobacionDocumentoBinding
+    private lateinit var binding: ActivityDetalleAprobacionDocumentoBinding
     private val PICK_IMAGE_REQUEST = 1
     private val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1
 
@@ -23,13 +28,32 @@ class DetalleAprobacionDocumentoActivity : AppCompatActivity() {
         initValues()
     }
 
-    private fun initValues(){
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            // Obtiene la URI de la imagen seleccionada
+            val selectedImageUri = data.data!!
+
+            // Convierte la URI a un bitmap
+            val bitmap =
+                BitmapFactory.decodeStream(contentResolver.openInputStream(selectedImageUri))
+
+            // Convierte el bitmap a base64
+            val base64Image = convertBitmapToBase64(bitmap)
+
+            println("Holi\n $base64Image")
+        }
+    }
+
+    private fun initValues() {
         // Cambiar el título de la barra de navegación y mostrarla
         supportActionBar?.title = "Detalle de Aprobación"
         supportActionBar?.show()
 
         // Obtener el objeto enviado desde la actividad anterior
-        val aprobacionDocumento = intent.getSerializableExtra("aprobacionDocumento") as AprobacionDocumento
+        val aprobacionDocumento =
+            intent.getSerializableExtra("aprobacionDocumento") as AprobacionDocumento
 
         // Mostrar los datos en los componentes
         binding.tvNumero.text = aprobacionDocumento.NumeroDocumento
@@ -47,11 +71,13 @@ class DetalleAprobacionDocumentoActivity : AppCompatActivity() {
 
     }
 
-    private fun initEvents(){
+    private fun initEvents() {
+        // Evento para abrir la galería
         binding.btnImagen.setOnClickListener {
             // Antes de abrir la galería, verifica si tienes el permiso
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED
+            ) {
                 // Si no tienes el permiso, solicítalo al usuario
                 ActivityCompat.requestPermissions(
                     this,
@@ -60,9 +86,18 @@ class DetalleAprobacionDocumentoActivity : AppCompatActivity() {
                 )
             } else {
                 // Si ya tienes el permiso, abre la galería
-                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                val intent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 startActivityForResult(intent, PICK_IMAGE_REQUEST)
             }
         }
+    }
+
+    // Método para convertir un bitmap a una cadena de base64
+    private fun convertBitmapToBase64(bitmap: Bitmap): String {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val byteArray = byteArrayOutputStream.toByteArray()
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
     }
 }
